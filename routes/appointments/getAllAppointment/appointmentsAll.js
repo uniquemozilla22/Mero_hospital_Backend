e646bf1aa1fe61e7363b1f52508c8bc346190456
uniquemozilla22 <uniqgaming26@gmail.com>
2021-06-10 00:01:06 +0545

    The Appointment for the User filtered is done
    Some Limitation of the data flow has been added to the Server for the Appointment

--- routes/appointments/getAllAppointment/appointmentsAll.js
+++ routes/appointments/getAllAppointment/appointmentsAll.js
@@ -1,18 +1,17 @@
-const {Appointments} =require("../../../database/Schema/Schema")
+const { Appointments } = require("../../../database/Schema/Schema");
 
+const appointments_all = (req, res) => {
+  Appointments.find({ user: req.user.id })
+    .populate("feild")
+    .select("-user")
+    .then((appointments) => {
+      res.send(appointments);
+    })
+    .catch((err) => {
+      res.send({
+        error: "There is a server error in fetching appointments : " + err,
+      });
+    });
+};
 
-
-const appointments_all =(req,res)=>{
-     Appointments.find({user:req.user.id})
-     .then(appointments=>{
-         res.send(appointments)
-     })
-     .catch(err=>{
-         res.send({error:"There is a server error in fetching appointments : "+err})
-     })
-
-
-}
-
-
-module.exports=appointments_all
+module.exports = appointments_all;
